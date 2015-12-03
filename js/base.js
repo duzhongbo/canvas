d8a01cbcdc1be4c88c28a3658fdf2c4bfcb5956c
duzhongbo <710409599@qq.com>
2015-12-03 15:42:07 +0800

    在画布中绘制文案

--- js/base.js
+++ js/base.js
@@ -1,9 +1,14 @@
-function showTxt(sTxt,oDom){
-	var context = oDom.getContext('2d');
+/*
+ * 作用：绘制指定文案
+ * 参数：sTxt：文案；oCanvas：画布
+ *
+*/
+function showTxt(sTxt,oCanvas){
+	var context = oCanvas.getContext('2d');
 	context.font = '38pt Arial';
 	context.fillStyle = '#f00';// 填充
 	context.strokeStyle = '#0f0';// 描边
-	context.fillText(sTxt,oDom.width/2-150,oDom.height/2 + 15);
-	context.strokeText(sTxt,oDom.width/2-150,oDom.height/2+15);
+	context.fillText(sTxt,oCanvas.width/2-150,oCanvas.height/2 + 15);
+	context.strokeText(sTxt,oCanvas.width/2-150,oCanvas.height/2+15);
 
 }
