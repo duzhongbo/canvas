72564665a3dff0c07257284af8260d9db40f550d
duzhongbo <710409599@qq.com>
2015-12-03 21:18:29 +0800

    绘制直角矩形

--- js/base.js
+++ js/base.js
@@ -1,5 +1,5 @@
 /*
- * 作用：绘制指定文案
+ * 作用：在指定画布中间，绘制指定文案
  * 参数：sTxt：文案；oCanvas：画布
  *
 */
@@ -11,4 +11,13 @@ function showTxt(sTxt,oCanvas){
 	context.fillText(sTxt,oCanvas.width/2-150,oCanvas.height/2 + 15);
 	context.strokeText(sTxt,oCanvas.width/2-150,oCanvas.height/2+15);
 
+}
+/*
+ * 功能：在指定画布，指定位置,绘制指定边长矩形
+ * 参数：nLength:边长；x:x坐标；y:y坐标；oCanvas：画布
+ *
+*/
+function drawRect(nLength,x,y,oCanvas){
+	var context = oCanvas.getContext('2d');
+	context.fillRect(x,y,nLength,nLength);
 }
