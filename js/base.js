b5d40323b35f6c612b56f0a501ad6a7a53ab6857
duzhongbo <710409599@qq.com>
2015-12-04 12:16:56 +0800

    颜色透明度

--- js/base.js
+++ js/base.js
@@ -11,11 +11,16 @@ function fillText(sTxt,oCanvas){
 }
 /*
  * 功能：在指定画布，指定位置,绘制指定边长实心矩形
- * 参数：nLength:边长；x:x坐标；y:y坐标；oCanvas：画布
- *
+ * 参数：
+ *      nLength:边长；
+ *      x:x坐标；
+ *      y:y坐标；
+ *      oCanvas：画布
+ *      alpha：透明度
 */
-function fillRect(nLength,x,y,oCanvas){
+function fillRect(nLength,x,y,oCanvas,alpha){
 	var context = oCanvas.getContext('2d');
+    fillStyle('#f00',context,0.5);
 	context.fillRect(x,y,nLength,nLength);
 }
 /*
@@ -38,9 +43,26 @@ function clearRect(x,y,w,h,oCanvas){
 	var context = oCanvas.getContext('2d');
 	context.clearRect(0,0,oCanvas.width,oCanvas.height);
 }
+/*
+ * 功能：设置填充的颜色、透明度
+ * 参数：
+ *      sColor：16进制颜色值
+ *      oContext：绘制语境
+ *      alpha：透明度,0~之间小数
+*/
+function fillStyle(sColor,oContext,alpha){
+    var color;
+    if(alpha){
+        var aRgb = sColor.colorRgb();
+        color = 'rgba('+aRgb[0]+','+aRgb[1]+','+aRgb[2]+','+alpha+')';
+    }else{
+        color = sColor;
+    }
+    oContext.fillStyle=color;
+}
 /*16进制颜色转为RGB格式*/
- String.prototype.colorRgb = function(){
- 	//十六进制颜色值的正则表达式
+String.prototype.colorRgb = function(){
+ 	// 十六进制颜色值的正则表达式
 	var reg = /^#([0-9a-fA-f]{3}|[0-9a-fA-f]{6})$/;
     var sColor = this.toLowerCase();
     if(sColor && reg.test(sColor)){
@@ -51,7 +73,7 @@ function clearRect(x,y,w,h,oCanvas){
                 }
                 sColor = sColorNew;
         }
-        //处理六位的颜色值
+        // 处理六位的颜色值
         var sColorChange = [];
         for(var i=1; i<7; i+=2){
             sColorChange.push(parseInt("0x"+sColor.slice(i,i+2)));        
