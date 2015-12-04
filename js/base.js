8de0f3b0bf7ec957f32db17ab0c4eeeebaf64d90
duzhongbo <710409599@qq.com>
2015-12-04 11:51:50 +0800

    16进制颜色转rgb

--- js/base.js
+++ js/base.js
@@ -38,3 +38,26 @@ function clearRect(x,y,w,h,oCanvas){
 	var context = oCanvas.getContext('2d');
 	context.clearRect(0,0,oCanvas.width,oCanvas.height);
 }
+/*16进制颜色转为RGB格式*/
+ String.prototype.colorRgb = function(){
+ 	//十六进制颜色值的正则表达式
+	var reg = /^#([0-9a-fA-f]{3}|[0-9a-fA-f]{6})$/;
+    var sColor = this.toLowerCase();
+    if(sColor && reg.test(sColor)){
+        if(sColor.length === 4){
+            var sColorNew = "#";
+                for(var i=1; i<4; i+=1){
+                    sColorNew += sColor.slice(i,i+1).concat(sColor.slice(i,i+1));        
+                }
+                sColor = sColorNew;
+        }
+        //处理六位的颜色值
+        var sColorChange = [];
+        for(var i=1; i<7; i+=2){
+            sColorChange.push(parseInt("0x"+sColor.slice(i,i+2)));        
+        }
+        return sColorChange;
+    }else{
+        return sColor;        
+	}
+};
