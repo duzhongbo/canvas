7a7c9cedab39ccf29dc7681e2b6d365469fdf470
duzhongbo <710409599@qq.com>
2015-12-04 11:29:34 +0800

    绘制空心矩形，擦除方法，代码整理

--- js/base.js
+++ js/base.js
@@ -3,21 +3,38 @@
  * 参数：sTxt：文案；oCanvas：画布
  *
 */
-function showTxt(sTxt,oCanvas){
+function fillText(sTxt,oCanvas){
 	var context = oCanvas.getContext('2d');
 	context.font = '38pt Arial';
 	context.fillStyle = '#f00';// 填充
-	context.strokeStyle = '#0f0';// 描边
 	context.fillText(sTxt,oCanvas.width/2-150,oCanvas.height/2 + 15);
-	context.strokeText(sTxt,oCanvas.width/2-150,oCanvas.height/2+15);
-
 }
 /*
- * 功能：在指定画布，指定位置,绘制指定边长矩形
+ * 功能：在指定画布，指定位置,绘制指定边长实心矩形
  * 参数：nLength:边长；x:x坐标；y:y坐标；oCanvas：画布
  *
 */
-function drawRect(nLength,x,y,oCanvas){
+function fillRect(nLength,x,y,oCanvas){
 	var context = oCanvas.getContext('2d');
 	context.fillRect(x,y,nLength,nLength);
+}
+/*
+ * 功能：在指定画布，指定位置,绘制指定边长空心矩形
+ * 参数：nLength:边长；x:x坐标；y:y坐标；oCanvas：画布
+ *
+*/
+function strokeRect(nLength,x,y,oCanvas){
+	var context = oCanvas.getContext('2d');
+	// context.lineJoin = 'round';
+	// context.lineWidth = 30;
+	context.strokeRect(x,y,nLength,nLength);
+}
+/*
+ * 功能：擦除指定坐标，指定尺寸的矩形区域
+ * 参数：x,y坐标，w:宽，h:高，oCanvas：画布
+ *
+*/
+function clearRect(x,y,w,h,oCanvas){
+	var context = oCanvas.getContext('2d');
+	context.clearRect(0,0,oCanvas.width,oCanvas.height);
 }
